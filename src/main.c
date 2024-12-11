0b533230edfe9122032c161813d33a5bfb892948
Nicolas Janis <Nicolas.d.janis@gmail.com>
2024-12-11 18:37:30 -0500

    update: main.c with basic SDL2 template

--- src/main.c
+++ src/main.c
@@ -1,23 +1,43 @@
-#include <SDL2/SDL.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL.h>
+#include <SDL_timer.h>
 #include <stdint.h>
 
-int main(int argc, char **argv) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL_Init Error : %s\n", SDL_GetError());
+int main(void) {
+    // Attempt to initialize graphics and timer system.
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window *win = SDL_CreateWindow("Hello SDL", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-    if (!win) {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+    SDL_Window *window = SDL_CreateWindow(
+        "I _ Chinese",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640, 480, 0
+    );
+    if (!window) {
+        printf("error creating window: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
 
-    SDL_Delay(3000);
-    SDL_DestroyWindow(win);
+    // Keep the window open, in this case SDL_Delay(5000); statement won't work.
+    bool running = true;
+    while (running) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+            {
+                running = false;
+                break;
+            }
+        }
+     }
+
+    // clean up resources before exiting.
+    SDL_DestroyWindow(window);
     SDL_Quit();
-    
-    return 0;
 }
