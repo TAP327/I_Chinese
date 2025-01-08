8ae5a5ee649b8c95a0fea4af440a64c8b7e60453
Nicolas Janis <Nicolas.d.janis@gmail.com>
2025-01-08 11:33:25 -0500

    update: Tests for db loader test suite

--- tests/db/test_db.c
+++ tests/db/test_db.c
@@ -1,6 +1,23 @@
 #include <criterion/criterion.h>
 #include <stdbool.h>
+#include "include/db/db.h"
 
-Test(db_loader, init_engine) {
-    cr_expect(true == true);
+sqlite3 *db_handle = NULL;
+
+void db_setup(void) {
+    db_handle = init_db_engine("./assets/i_chinese.db");
+}
+
+void db_teardown(void) {
+    close_db_engine(db_handle);
+}
+
+TestSuite(db_loader_suite, .init=db_setup, .fini=db_teardown);
+
+Test(db_loader_suite, init_engine) {
+    cr_expect(db_handle != NULL, "db_handle should not be null");
+}
+
+Test(db_loader_suite, validate_schema) {
+    cr_expect(validate_db(db_handle) == SQLITE_OK, "Invalid DB schema");
 }
