93e059d6c183df5e64e65c4d80340e7ce9f09294
Taylor Perry <taylorperry327@gmail.com>
2025-01-11 13:41:44 -0500

    Update: graphics contains get_txt_texture, get_proportional_pos, destroy_TextTexture, render_txt

--- include/graphics/graphics.h
+++ include/graphics/graphics.h
@@ -1,8 +1,22 @@
 #ifndef _GRAPHICS_H
 #define _GRAPHICS_H
 
+typedef struct {
+    SDL_Texture *texture;
+    int width;
+    int height;
+} TextTexture;
+
+typedef struct {
+    int x;
+    int y;
+} Position;
+
+
 void set_bg(SDL_Renderer *renderer);
-SDL_Texture *get_txt_texture(SDL_Renderer *renderer, const char *text, SDL_Color *color, const char *file, const int size);
-void render_txt(void);
+TextTexture *get_txt_texture(SDL_Renderer *renderer, const char *text, SDL_Color *color, const char *file, const int size);
+Position *get_proportional_pos(int down_shift, int right_shift);
+void render_txt(SDL_Renderer *renderer, TextTexture *texture, Position *pos);
+void destory_TextTexture (TextTexture *txt_texture);
 
 #endif 
--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -9,22 +9,68 @@ void set_bg(SDL_Renderer *renderer) {
     SDL_RenderPresent(renderer);
 }
 
-SDL_Texture *get_txt_texture(SDL_Renderer *renderer, const char *text, SDL_Color *color, const char *file, const int size) {
+TextTexture *get_txt_texture(SDL_Renderer *renderer, const char *text, SDL_Color *color, const char *file, const int size) {
     TTF_Font *font = TTF_OpenFont(file, size);
 
-    SDL_Surface *surface = TTF_RenderUTF8_Blended(font, text, *color);
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    TextTexture *txt_texture = {
+        NULL,
+        0,
+        0
+    };
 
-    return texture;
+    SDL_Surface *surface = TTF_RenderUTF8_Blended(font, text, *color);
+    if (!surface) {
+        fprintf(stderr, "Failed to render text surface: %s\n", TTF_GetError());
+        return txt_texture;
+    }
+
+    txt_texture->texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (!txt_texture->texture) {
+        fprintf(stderr, "Failed to create texture from surface: %s\n", SDL_GetError());
+        return txt_texture;
+    }
+
+    txt_texture->height = surface->h;
+    txt_texture->width = surface->w;
+
+    return txt_texture;
 }
 
-void render_txt(void) {
+Position *get_proportional_pos(int down_shift, int right_shift) {
+    Position *pos = {
+        0,
+        0
+    };
+    
     SDL_DisplayMode DM;
     if (SDL_GetCurrentDisplayMode(0, &DM) == 0) {
         int hs_h = DM.h;
         int hs_w = DM.w;
+    
+        pos->x = hs_w*right_shift;
+        pos->y = hs_h*down_shift;   
     }
     else {
         printf("Home screen dimensions not found.");
+    }
+
+    return pos;
+}
+
+void render_txt(SDL_Renderer *renderer, TextTexture *texture, Position *pos) {
+    if(!renderer || !texture || !texture->texture || !pos) {
+        fprintf(stderr, "Invalid parameters passed to render_txt.\n");
+        return;
+    }
+    SDL_Rect *dest_rect = {pos->x, pos->y, texture->width, texture->height};
+    if (SDL_RenderCopy(renderer,texture->texture, NULL, dest_rect) != 0) {
+        fprintf(stderr, "SDL_RenderCopy failed to render text: %s\n", SDL_GetError());
+    }
+}
+
+void destory_TextTexture (TextTexture *txt_texture) {
+    if(txt_texture && txt_texture->texture) {
+        SDL_DestroyTexture(txt_texture->texture);
+        txt_texture->texture = NULL;
     }
 }
